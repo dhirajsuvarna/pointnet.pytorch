f7d34dcd102b282eecd40726588092a53c54218d
unknown <dhiraj.suvarna@gmail.com>
2020-04-20 15:46:38 +0530

    changing train_test generation script to include few cases.

--- dataset_utils/pcd_utils/create_train_test_shuffle.py
+++ dataset_utils/pcd_utils/create_train_test_shuffle.py
@@ -2,7 +2,7 @@
 # 1. train_test_split.json - file containing train and test split of dataset
 # 2. classlabels.txt - file containing Class Names or Category Names
 
-# output will be of the following format 
+# output will be of the following format if the "--labeled_split flag is True"
 # {
 #   train: {
 #       file1:label-A
@@ -12,6 +12,20 @@
 #   test: {
 #       file1:label-A
 #       file2:label-B
+#       ...
+#   }
+# }
+
+# output will be of the following format if the "--labeled_split flag is False"
+# {
+#   train: {
+#       file1
+#       file2
+#       ...
+#   }
+#   test: {
+#       file1
+#       file2
 #       ...
 #   }
 # }
@@ -33,55 +47,78 @@ import math
 import json
 import argparse
 
-parser = argparse.ArgumentParser()
 
-parser.add_argument("--input_path", required=True, help="Path to the Dataset Folder")
-parser.add_argument("--split_percent", default=80, help="Split Percentage between Train and Test")
-
-args = parser.parse_args()
-
-# datasetPath = r"F:\projects\ai\pointnet\dataset\DMUNet_OBJ_format\dataset_PCD_5000"
-# splitPercent = 80
-
-datasetPath = args.input_path
-splitPercent = args.split_percent
-
-outputFilePath = os.path.join(datasetPath, "train_test_split.json")
-classLabelFilePath = os.path.join(datasetPath, "classlabels.txt")
-
-train_test_Dict = {"train": {}, "test": {}}
-classLabels = []
-
-for labelDir in os.listdir(datasetPath):
-    labelDirPath = os.path.join(datasetPath, labelDir)
-    if os.path.isdir(labelDirPath):
-        files = os.listdir(labelDirPath)
+def create_split(iDirPath, iSplitPercent, ext):
+    print(f"Creating split of : {iDirPath}")
+    if os.path.isdir(iDirPath):
+        files = [f for f in os.listdir(iDirPath) if f.endswith(ext)]
 
         random.shuffle(files)
-        files = [os.path.join(datasetPath, labelDir, f) for f in files]
+        files = [os.path.join(iDirPath, f) for f in files]
         
         numFiles = len(files)
-        splitIndex = math.floor(numFiles * (splitPercent/100.0))
+        splitIndex = math.floor(numFiles * (iSplitPercent/100.0))
 
         trainFiles = files[:splitIndex]
         testFiles = files[splitIndex:]
 
-        train_test_Dict["train"].update({f:labelDir for f in trainFiles})
-        train_test_Dict["test"].update({f:labelDir for f in testFiles})
+        return trainFiles, testFiles
 
-        classLabels.append(labelDir)
-        print(f"debug: {labelDir} => totalFiles: {numFiles}, train: {len(trainFiles)} + test: {len(testFiles)} = {len(trainFiles) + len(testFiles)} ")
+def create_split_with_labels(datasetPath, outputFilePath, splitPercent, classLabelFilePath, ext):
 
-with open(outputFilePath, 'w') as outFile:    
-    json.dump(train_test_Dict, outFile, indent=4)
+    train_test_Dict = {"train": {}, "test": {}}
+    classLabels = []
 
-with open(classLabelFilePath, 'w') as classLabelFile:
-    for label in classLabels:
-        classLabelFile.write('%s\n', label)
+    for labelDir in os.listdir(datasetPath):
+        labelDirPath = os.path.join(datasetPath, labelDir)
+        if os.path.isdir(labelDirPath):
+            trainFiles, testFiles = create_split(labelDirPath, splitPercent, ext)
+            train_test_Dict["train"].update({f:labelDir for f in trainFiles})
+            train_test_Dict["test"].update({f:labelDir for f in testFiles})
+
+            classLabels.append(labelDir)
+
+    with open(outputFilePath, 'w') as outFile:    
+        json.dump(train_test_Dict, outFile, indent=4)
+
+    with open(classLabelFilePath, 'w') as classLabelFile:
+        for label in classLabels:
+            classLabelFile.write('%s\n' %label)
 
 
+def create_split_without_label(datasetPath, outputFilePath, splitPercent, ext):
+    train_test_Dict = {"train": [], "test": []}
+
+    for labelDir in os.listdir(datasetPath):
+        labelDirPath = os.path.join(datasetPath, labelDir)
+        if os.path.isdir(labelDirPath):
+            trainFiles, testFiles = create_split(labelDirPath, splitPercent, ext)
+            train_test_Dict["train"] = train_test_Dict["train"] + trainFiles
+            train_test_Dict["test"] = train_test_Dict["test"] + testFiles
+
+    with open(outputFilePath, 'w') as outFile:    
+        json.dump(train_test_Dict, outFile, indent=4)
 
 
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
 
+    parser.add_argument("--input", required=True, help="Path to the Dataset Folder")
+    parser.add_argument("--split_percent", type=int, default=80, help="Split Percentage between Train and Test")
+    parser.add_argument("--ext", required=True, help="Extension of the files to be included")
+    parser.add_argument("--labeled_split", action='store_true', help="Creates split with labels i.e 'Training_Example:Label'")
 
+    args = parser.parse_args()
+    print(f"Arguments:{args}")
 
+    datasetPath = args.input
+    splitPercent = args.split_percent
+    ext = args.ext
+
+    outputFilePath = os.path.join(datasetPath, "train_test_split.json")
+    classLabelFilePath = os.path.join(datasetPath, "classlabels.txt")
+
+    if args.labeled_split:
+        create_split_with_labels(datasetPath, outputFilePath, splitPercent, classLabelFilePath, ext)
+    else:
+        create_split_without_label(datasetPath, outputFilePath, splitPercent, ext)
